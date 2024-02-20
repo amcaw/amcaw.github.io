eacda9b66758bbf920b4dc560b8c18168c12e234
amcaw <42608053+amcaw@users.noreply.github.com>
2024-02-20 21:55:20 +0100

    Create Bloc_2.svelte

---
+++ src/Bloc_2.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Scrolly from './Scrolly.svelte';
+
+  let stepsData=[
+  {"id": 1, "text": "First step text", "desktopImage": "./pics/epicon.jpeg", "mobileImage": "./pics/epicon.jpeg"},
+  {"id": 2, "text": "Second step text", "desktopImage": "./pics/oxagon.jpeg", "mobileImage": "mobileImage2.jpg"},
+  {"id": 3, "text": "Third step text", "desktopImage": "./pics/trojena.jpeg", "mobileImage": "mobileImage3.jpg"}
+];
+</script>
+
+<Scrolly {stepsData} />
+
+
+
